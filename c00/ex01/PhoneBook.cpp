75440db2249b7697a056427d166388107bc93888
AnassAnbadi <anassanbadi69@gmail.com>
2025-12-26 01:25:47 +0100

    refactor: remove PhoneBook class methods for better code clarity

--- c00/ex01/PhoneBook.cpp
+++ c00/ex01/PhoneBook.cpp
@@ -1,4 +1,39 @@
 #include "PhoneBook.hpp"
+
+bool is_space(char c)
+{
+    return (c == ' ' || c == '\t' || c == '\n' || c == '\r');
+}
+
+bool isOnlySpaces(const std::string s)
+{
+    size_t i = 0;
+
+    if (s.empty())
+        return true;
+
+    while (i < s.size())
+    {
+        if (!is_space(s[i]))
+            return false;
+        i++;
+    }
+    return true;
+}
+
+bool ASCII_CHECK(std::string s)
+{
+    size_t i = 0;
+
+    while (i < s.size())
+    {
+        if (isascii(s[i]) == 0)
+            return false;
+        i++;
+    }
+    return true;
+}
+
 int PhoneBook::contactCount = 0;
 void PhoneBook::addContact() {
     std::string fname, lname, nname, pnumber, secret;
@@ -9,16 +44,36 @@ void PhoneBook::addContact() {
 
     std::cout << "Enter First Name: ";
     std::getline(std::cin, fname);
+    if (isOnlySpaces(fname)) {
+        std::cout << "First Name cannot be empty or only spaces!" << std::endl;
+        return;
+    }
     std::cout << "Enter Last Name: ";
     std::getline(std::cin, lname);
+    if (isOnlySpaces(lname)) {
+        std::cout << "Last Name cannot be empty or only spaces!" << std::endl;
+        return;
+    }
     std::cout << "Enter Nickname: ";
     std::getline(std::cin, nname);
+    if (isOnlySpaces(nname)) {
+        std::cout << "Nickname cannot be empty or only spaces!" << std::endl;
+        return;
+    }
     std::cout << "Enter Phone Number: ";
     std::getline(std::cin, pnumber);
+    if (isOnlySpaces(pnumber)) {
+        std::cout << "Phone Number cannot be empty or only spaces !" << std::endl;
+        return;
+    }
     std::cout << "Enter Darkest Secret: ";
     std::getline(std::cin, secret);
+    if (isOnlySpaces(secret)) {
+        std::cout << "Darkest Secret cannot be empty or only spaces!" << std::endl;
+        return;
+    }
 
-    contacts[contactCount].setContact(fname, lname, nname, pnumber, secret);
+    contacts[contactCount%8].setContact(fname, lname, nname, pnumber, secret);
     contactCount++;
 }
 void PhoneBook::displayContacts() const {
